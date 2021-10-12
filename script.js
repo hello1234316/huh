8bb81de899c1769454468c88145e2b5677f3c24d
HELLO Phone <85028651+hello1234316@users.noreply.github.com>
2021-10-12 15:38:34 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const typed = new Typed(".typing", {
+strings: [
+"🐧 Verified DIscord Bot Developer", 
+"🐔 Web Development Newbie.", 
+"🌌 Html,Css,Js,Python."
+],
+typeSpeed: 75,
+backSpeed: 50,
+});
+0,
+});
